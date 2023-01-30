cb863839ebe5eee849121c3fb11e8e7704060f5e
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-01-30 21:39:06 +0400

    New

---
+++ C#/CSharp_Project/CSharp_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.InputEncoding = System.Text.Encoding.UTF8;
+
+namespace Main
+{
+    class Menu
+    {
+        Menu() { }
+
+        void StartMenu()
+        {
+            Console.WriteLine("~--Menu--~\n" +
+                "1 - Create Dictionary\n" +
+                "2 - Edit Dictionary\n" +
+                "3 - Search The Word\n" +
+                "4 - Save To File\n" +
+                "5 - Exit Program\n"
+                );
+        }
+    }
+}
+
+
